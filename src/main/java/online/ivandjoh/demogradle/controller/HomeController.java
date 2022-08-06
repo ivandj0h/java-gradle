0c0a06eaf9cb0d62bc2f200604ed50de6dcbb312
ivandjoh <ivandi.djoh@gmail.com>
2022-08-06 09:25:26 +0800

    create : 1. controller 2. entity 3. service & service impl 4. repository

--- src/main/java/online/ivandjoh/demogradle/controller/HomeController.java
+++ src/main/java/online/ivandjoh/demogradle/controller/HomeController.java
@@ -4,35 +4,47 @@ import online.ivandjoh.demogradle.Entity.User;
 import online.ivandjoh.demogradle.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HomeController {
 
 	@Autowired
 	UserService userService;
 
+	@GetMapping("")
 	public ResponseEntity<?> homeController() {
 		return ResponseEntity.ok("Hello World");
 	}
 
+	@PostMapping("/user")
 	public ResponseEntity<?> createUser(User user) {
+
 		return userService.createUser(user);
 	}
 
+	@GetMapping("/users/all")
 	public ResponseEntity<?> getAllUsers() {
+
 		return userService.getAllUsers();
 	}
 
+	@GetMapping("/users/id")
 	public ResponseEntity<?> getUserById(Long id) {
+
 		return userService.getUserById(id);
 	}
 
+	@PutMapping("/users/id")
 	public ResponseEntity<?> updateUser(Long id, User user) {
+
 		return userService.updateUser(id, user);
 	}
 
+	@DeleteMapping("/users/id")
 	public ResponseEntity<?> deleteUser(Long id) {
+
 		return userService.deleteUser(id);
 	}
 }
--- src/main/java/online/ivandjoh/demogradle/service/Impl/UserServiceImpl.java
+++ src/main/java/online/ivandjoh/demogradle/service/Impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package online.ivandjoh.demogradle.service.Impl;
 
 import online.ivandjoh.demogradle.Entity.User;
+import online.ivandjoh.demogradle.repository.UserRepository;
 import online.ivandjoh.demogradle.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -10,28 +12,68 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public ResponseEntity<List<User>> getAllUsers() {
-        return null;
+        try {
+            List<User> users = userRepository.findAll();
+
+            return ResponseEntity.ok(users);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Override
     public ResponseEntity<User> getUserById(Long id) {
-        return null;
+        try {
+            User user = userRepository.findById(id).orElse(null);
+
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Override
     public ResponseEntity<?> updateUser(Long id, User user) {
-        return null;
+        try {
+            User userToUpdate = userRepository.findById(id).orElse(null);
+
+            assert userToUpdate != null;
+            userToUpdate.setName(user.getName());
+            userToUpdate.setEmail(user.getEmail());
+            userToUpdate.setAge(user.getAge());
+            userToUpdate.setSalary(user.getSalary());
+            userRepository.save(userToUpdate);
+
+            return ResponseEntity.ok(userToUpdate);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Override
     public ResponseEntity<?> createUser(User user) {
-        return null;
+        try {
+            User newUser = userRepository.save(user);
+
+            return ResponseEntity.ok(newUser);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Override
     public ResponseEntity<?> deleteUser(Long id) {
-        return null;
+        try {
+            userRepository.deleteById(id);
+
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
